3076739768e30b646c9d3e91e04c3e1f32589a4e
Shengbo.Xu <shengbo.xu@medley.jp>
2017-08-17 18:41:41 +0900

    add: contents 2017年 8月17日 木曜日 18時41分41秒 JST

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# bangladesh-engineer
-バングラディッシュエンジニアのサイトです。
+# my_cafe
---
+++ source/javascripts/all.js
@@ -0,0 +1,15 @@
+//= require vendor/jquery.min
+//= require vendor/jquery-migrate.min
+//= require vendor/bootstrap.min
+
+//= require vendor/jquery.easing.js
+//= require vendor/jquery.back-to-top
+//= require vendor/jquery.wow.min
+//= require vendor/swiper/js/swiper.jquery.min
+//= require vendor/masonry/jquery.masonry.pkgd.min
+//= require vendor/masonry/imagesloaded.pkgd.min
+
+//= require vendor/js/layout.min
+//= require vendor/js/components/wow.min
+//= require vendor/js/components/swiper.min
+//= require vendor/js/components/masonry.min
